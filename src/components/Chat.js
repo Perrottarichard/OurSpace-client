9689283e4701adf2004efb9ced70e5e6f1c3cb57
Richard Perrotta <perrottarichard@gmail.com>
2020-08-28 16:35:45 +0700

    remove client transports

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -49,7 +49,7 @@ const Chat = ({ location }) => {
     //why isn't this working
 
     if (!client) {
-      socket = io('https://r-space-server.herokuapp.com/', { transports: ['websocket', 'polling'], secure: true })
+      socket = io('https://r-space-server.herokuapp.com/', { secure: true })
       setClient(true)
     }
 
